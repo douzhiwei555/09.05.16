5426f43aa7cfa3e099ec04525d6fd97752b823f9
admin <abc@abc.com>
2016-09-05 20:30:05 +0800

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+require("../../bower_components/Swiper/dist/css/swiper.min.css")
+var Swiper = require("../../bower_components/Swiper/dist/js/swiper.min.js");
+//实例化
+new Swiper(".swiper-container");
